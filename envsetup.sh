03119431dacc7514dc5619b9895280607280a79c
Syed Sajid <syedsajid341@gmail.com>
2018-10-28 22:39:41 +0000

    fix typo

--- envsetup.sh
+++ envsetup.sh
@@ -137,8 +137,8 @@ function check_product()
         echo "Couldn't locate the top of the tree.  Try setting TOP." >&2
         return
     fi
-    if (echo -n $1 | grep -q -e "^superior_") ; then
-        CUSTOM_BUILD=$(echo -n $1 | sed -e 's/^superior_//g')
+    if (echo -n $1 | grep -q -e "^venom_") ; then
+        CUSTOM_BUILD=$(echo -n $1 | sed -e 's/^venom_//g')
         export BUILD_NUMBER=$( (date +%s%N ; echo $CUSTOM_BUILD; hostname) | openssl sha1 | sed -e 's/.*=//g; s/ //g' | cut -c1-10 )
     else
         CUSTOM_BUILD=
